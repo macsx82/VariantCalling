e91822b4726c06f2edbfe33c6058e01961923086
macsx82 <macsx82@gmail.com>
2018-09-07 13:50:16 +0200

    fixed step 19

--- 1919.GATK4_step1919.sh
+++ 1919.GATK4_step1919.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 echo
 echo "> pipeline: Η Σκύλλα και η Χάρυβδη"
@@ -13,17 +14,21 @@ iVR="${variantdb}_rawHFSO-iVR.vcf"
 tri="${variantdb}_indel.tranches"
 mode_I=INDEL
 ### - SOURCEs - ###
-source /home/manolis/GATK4/gatk4path.sh
+param_file=$1
+source ${param_file}
+#source functions file
+own_folder=`dirname $0`
+source ${own_folder}/pipeline_functions.sh
 ### - CODE - ###
 
 #19
 echo
-cd ${fol8}/${variantdb}/
+# cd ${fol8}/${variantdb}/
 echo "> Indel Variant Recalibrator"
-${GATK4} --java-options ${java_opt3x} VariantRecalibrator -V ${SO} -O ${iVR} --tranches-file ${tri} --trust-all-polymorphic -tranche "100.0" -tranche "99.95" -tranche "99.9" -tranche "99.5" -tranche "99.0" -tranche "97.0" -tranche "96.0" -tranche "95.0" -tranche "94.0" -tranche "93.5" -tranche "93.0" -tranche "92.0" -tranche "91.0" -tranche "90.0" -an "FS" -an "ReadPosRankSum" -an "MQRankSum" -an "QD" -an "SOR" -mode ${mode_I} --max-gaussians 4 -resource mills,known=false,training=true,truth=true,prior=12:${OTGindels} -resource axiomPoly,known=false,training=true,truth=false,prior=10:${AXIOM} -resource dbsnp,known=true,training=false,truth=false,prior=2:${DBSNP138} 
+${GATK4} --java-options ${java_opt3x} VariantRecalibrator -V ${fol8}/${variantdb}/${SO} -O ${fol8}/${variantdb}/${iVR} --tranches-file ${fol8}/${variantdb}/${tri} --trust-all-polymorphic -tranche "100.0" -tranche "99.95" -tranche "99.9" -tranche "99.5" -tranche "99.0" -tranche "97.0" -tranche "96.0" -tranche "95.0" -tranche "94.0" -tranche "93.5" -tranche "93.0" -tranche "92.0" -tranche "91.0" -tranche "90.0" -an "FS" -an "ReadPosRankSum" -an "MQRankSum" -an "QD" -an "SOR" -mode ${mode_I} --max-gaussians 4 -resource mills,known=false,training=true,truth=true,prior=12:${OTGindels} -resource axiomPoly,known=false,training=true,truth=false,prior=10:${AXIOM} -resource dbsnp,known=true,training=false,truth=false,prior=2:${DBSNP138} 
 echo "- END -"
 
-exit
+touch step19.done
 
 # -an "DP" # excluded from WES
 
