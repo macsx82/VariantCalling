bba16c88b70490e975199576a71da780d46d9032
macsx82 <macsx82@gmail.com>
2018-09-07 13:33:57 +0200

    fixed step 09

--- 0909.GATK4_step0909.sh
+++ 0909.GATK4_step0909.sh
@@ -13,14 +13,18 @@ SM=$3					#sample name
 applybqsr="${SM}_bqsr.bam"		#final_merged apply recalibration report in bam
 c_gv="${SM}_${f2}_g.vcf.gz"		#conting gVCF file
 ### - SOURCEs - ###
-source /home/manolis/GATK4/gatk4path.sh
+param_file=$1
+source ${param_file}
+#source functions file
+own_folder=`dirname $0`
+source ${own_folder}/pipeline_functions.sh
 ### - CODE - ###
 
 #loop-9
 echo
-cd ${fol4}/
+# cd ${fol4}/
 echo "> HaplotypeCaller"
-${GATK4} --java-options ${java_opt2x} ${java_XX1} ${java_XX2} HaplotypeCaller -R ${GNMhg38} -I ${applybqsr} -O ${fol5}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF
+${GATK4} --java-options ${java_opt2x} ${java_XX1} ${java_XX2} HaplotypeCaller -R ${GNMhg38} -I ${fol4}/${applybqsr} -O ${fol5}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF
 echo "- END -"
 
 #qdel
@@ -29,7 +33,4 @@ rm -v ${fol4}/"${SM}_bqsr.bam"
 rm -v ${fol4}/"${SM}_bqsr.bai"
 rm -v ${fol4}/"${SM}_bqsr.bam.md5"
 
-exit
-
-
-
+touch step09.done
