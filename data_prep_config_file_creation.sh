97308177e838c88f93127f94caa12a9c957b6903
macsx82 <macsx82@gmail.com>
2018-09-11 18:40:30 +0200

    fixed args parsing in template config creation file

--- data_prep_config_file_creation.sh
+++ data_prep_config_file_creation.sh
@@ -236,7 +236,7 @@ fi
 suffix=`date +"%d%m%Y%H%M%S"`
 
 echo "${@}"
-while getopts ":t:o:s:h:m:f" opt ${@}; do
+while getopts ":t:o:s:h:m:i:f" opt ${@}; do
   case $opt in
     t)
       echo ${OPTARG}
