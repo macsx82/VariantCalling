8091a53bd43d2daafbc9d61401c1416bd82c9dcb
macsx82 <macsx82@gmail.com>
2018-09-13 02:16:55 +0200

    fixed template preparation

--- config_file_creation.sh
+++ config_file_creation.sh
@@ -135,6 +135,15 @@ passed="\${variantdb}_CohortOnlyPASS_Variants_PostVQSR.vcf"
 #final="${variantdb}_VQSR_output.vcf"
 #passed="${variantdb}_CohortOnlyPASS_Variants_PostVQSR.vcf"
 
+#########SET UP YOUR EMAIL HERE ##############
+mail=$3
+#########SET UP YOUR EMAIL HERE ##############
+
+#########SET UP SGE PARAMETERS HERE ##########
+sge_q=all
+seq_m=10G
+#########SET UP SGE PARAMETERS HERE ##########
+
 ###########################################################
 #---#
 java_opt1x='-Xmx5g'	#meroria java		# Chr12,Wg12,21x2
@@ -258,6 +267,62 @@ EOF
 
 }
 
+function build_runner_alignemt(){
+  param_file=$1
+
+cat << EOF
+#!/usr/bin/env bash
+
+echo
+echo "> pipeline: Η Σκύλλα και η Χάρυβδη"
+dt1=$(date '+%Y/%m/%d %H:%M:%S')
+echo "\$dt1"
+echo
+
+### - SOURCEs - ###
+#We will provide a different param file for each user, with variables and softwares paths as needed
+param_file=$1
+source ${param_file}
+#source functions file
+source \${hs}/pipeline_functions.sh
+
+#log folders creation
+mkdir -p \${lg}
+
+### - CODE - ###
+
+echo " --- START PIPELINE ---"
+echo
+
+#Pre-processing
+#pipe step 1
+#IN val OUT uBAM /// FastqToSam, ValidateSamFile, flagsta, view
+
+echo "bash \${hs}/0101.GATK4_step0101.sh ${SM}" | qsub -N G4s0101_\${SM} -cwd -l h_vmem=\${seq_m} -o \${lg}/g0101_\${SM}.log -e \${lg}/g0101_\${SM}.error -m a -M \${mail} -q \${sge_q}
+
+#Pre-processing
+#pipe step 2
+#IN uBAM OUT bBAM /// SamToFastq, ValidateSamFile, flagstat, view
+
+echo "bash \${hs}/0202.GATK4_step0202.sh ${SM}" | qsub -N G4s0202_\${SM} -cwd -l h_vmem=\${seq_m} -pe orte \${thr} -hold_jid G4s0101_\${SM} -o \${lg}/g0202_\${SM}.log -e ${lg}/g0202_\${SM}.error -m a -M ${mail} -q \${sge_q}
+
+#Pre-processing
+#pipe step 3-5
+#IN uBAM+bBAM OUT mBAM /// MergeBamAlignment, ValidateSamFile, flagstat, view
+#IN mBAM OUT mdBAM /// MarkDuplicates, ValidateSamFile, flagstat, view
+#IN mdBAM OUT fBAM /// SortSam, SetNmAndUqTags, ValidateSamFile, flagstat, view, sort, depth
+
+echo "bash \${hs}/0305.GATK4_step0305.sh ${SM}" | qsub -N G4s0305_\${SM} -cwd -l h_vmem=\${seq_m} -hold_jid G4s0202_\${SM} -o \${lg}/g0305_\${SM}.log -e \${lg}/g0305_\${SM}.error -m ea -M \${mail} -q \${sge_q}
+
+echo
+echo " --- END PIPELINE ---"
+
+exit
+
+EOF
+
+
+}
 
 
 if [ $# -lt 1 ]
