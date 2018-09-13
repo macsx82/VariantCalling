3ead1c3b5bc4c48e8f93b72f25bb98d8f29770ec
macsx82 <macsx82@gmail.com>
2018-09-13 16:51:06 +0200

    added additional runner builder and job array runner

---
+++ f_build_runner_LastSSel.sh
@@ -0,0 +1,59 @@
+function build_runner_LastSSel() {
+param_file=$1
+
+cat << EOF
+#!/usr/bin/env bash
+
+echo
+echo "> pipeline: Η Σκύλλα και η Χάρυβδη"
+dt1=\$(date '+%Y/%m/%d %H:%M:%S')
+echo "\$dt1"
+echo
+
+### - SOURCEs - ###
+#We will provide a different param file for each user, with variables and softwares paths as needed
+param_file=$1
+source ${param_file}
+#source functions file
+source \${hs}/pipeline_functions.sh
+
+#log folders creation
+mkdir -p \${lg}
+
+### - mkdir FOLDER / make FILE - ###
+mkdir \${lg}/\${variantdb}
+mkdir \${fol7}/\${variantdb}
+mkdir \${fol8}/\${variantdb}
+mkdir \${fol9}/\${variantdb}
+mkdir \${fol9}/\${variantdb}/xSamplePassedVariantsVCFs
+### - CODE - ###
+
+echo " --- START PIPELINE ---"
+echo
+
+#PASS variants selection
+#pipe step 22-24
+#IN cohort VQSR.vcf, cohort raw.vcf OUT pass.list, sample.list /// grep
+#IN cohort raw.vcf, pass.list OUT cohort-PASS-variants postVQSR /// SelectVariants
+#IN cohort-PASS-variants postVQSR, sample.list OUT per sample-PASS-variants postVQSR /// SelectVariants
+
+echo "bash \${hs}/2224.GATK4_step2224.sh \${param_file}" | qsub -N G4s2224_\${variantdb} -hold_jid G4s2121_\${variantdb} -o \${lg}/\${variantdb}/g2224_\${variantdb}_\\\$JOB_ID.log -e \${lg}/\${variantdb}/g2224_\${variantdb}_\\\$JOB_ID.error -m ea -M \${mail} -cwd -l h_vmem=\${sge_m} -q \${sge_q} 
+
+#Interval Filtered PASS variants selection
+#pipe step 25
+#IN sample-PASS-variants postVQSR, interval list OUT sample-Interval-Filtered-PASS-variants postVQSR ***Whole Genes /// SelectVariants
+#IN sample-PASS-variants postVQSR, interval list OUT sample-Interval-Filtered-PASS-variants postVQSR ***Whole Exons + 12bp each side /// SelectVariants
+
+echo "bash \${hs}/Chr2525.GATK4_step2525.sh \${param_file}" | qsub -N G4s2525_\${variantdb} -hold_jid G4s2224_\${variantdb} -o \${lg}/\${variantdb}/g2525_\${variantdb}_\\\$JOB_ID.log -e \${lg}/\${variantdb}/g2525_\${variantdb}_\\\$JOB_ID.error -m ea -M \${mail} -cwd -l h_vmem=\${sge_m} -q \${sge_q} 
+
+
+echo
+echo " --- END PIPELINE ---"
+
+exit
+
+
+EOF
+
+}
+
---
+++ f_build_runner_VQSR.sh
@@ -0,0 +1,67 @@
+function build_runner_VQSR() {
+param_file=$1
+
+cat << EOF
+#!/usr/bin/env bash
+
+echo
+echo "> pipeline: Η Σκύλλα και η Χάρυβδη"
+dt1=\$(date '+%Y/%m/%d %H:%M:%S')
+echo "\$dt1"
+echo
+
+### - SOURCEs - ###
+#We will provide a different param file for each user, with variables and softwares paths as needed
+param_file=$1
+source ${param_file}
+#source functions file
+source \${hs}/pipeline_functions.sh
+
+#log folders creation
+mkdir -p \${lg}
+
+### - mkdir FOLDER / make FILE - ###
+mkdir \${lg}/\${variantdb}
+mkdir \${fol7}/\${variantdb}
+mkdir \${fol8}/\${variantdb}
+mkdir \${fol9}/\${variantdb}
+mkdir \${fol9}/\${variantdb}/xSamplePassedVariantsVCFs
+### - CODE - ###
+
+echo " --- START PIPELINE ---"
+echo
+
+#Pre-VQSR
+#pipe step 17-18
+#IN cohort raw-VCF OUT cohort Hard Filtered raw-VCF /// VariantFiltration
+#IN cohort Hard Filtered raw-VCF OUT cohort Site Only raw-VCF /// MakeSitesOnlyVcf
+
+echo "bash \${hs}/1718.GATK4_step1718.sh \${param_file}" | qsub -N G4s1718_\${variantdb} -hold_jid G4s1616_\${variantdb} -o \${lg}/\${variantdb}/g1718_\${variantdb}_\\\$JOB_ID.log -e \${lg}/\${variantdb}/g1718_\${variantdb}_\\\$JOB_ID.error -m ea -M \${mail} -cwd -l h_vmem=\${sge_m} -q \${sge_q} 
+
+#VQSR
+#pipe step 19
+#IN Site cohort Site Only raw-VCF OUT indel tranches, indel recal /// VariantRecalibrator
+
+echo "bash \${hs}/1919.GATK4_step1919.sh \${param_file}" | qsub -N G4s1919_\${variantdb} -hold_jid G4s1718_\${variantdb} -o \${lg}/\${variantdb}/g1919_\${variantdb}_\\\$JOB_ID.log -e \${lg}/\${variantdb}/g1919_\${variantdb}_\\\$JOB_ID.error -m ea -M \${mail} -cwd -l h_vmem=\${sge_m_j3} -q \${sge_q} 
+
+#VQSR
+#pipe step 20
+#IN Site cohort Site Only raw-VCF OUT snp tranches, snp recal /// VariantRecalibrator
+
+echo "bash \${hs}/2020.GATK4_step2020.sh \${param_file}" | qsub -N G4s2020_\${variantdb} -hold_jid G4s1718_\${variantdb} -o \${lg}/\${variantdb}/g2020_\${variantdb}_\\\$JOB_ID.log -e \${lg}/\${variantdb}/g2020_\${variantdb}_\\\$JOB_ID.error -m ea -M \${mail} -cwd -l h_vmem=\${sge_m_j4} -q \${sge_q} 
+
+#VQSR
+#pipe step 21
+#IN Site cohort Site Only raw-VCF, tranche file, recal file OUT cohort VQSR vcf /// ApplyVQSR
+
+echo "bash \${hs}/2121.GATK4_step2121.sh \${param_file}" | qsub -N G4s2121_\${variantdb} -hold_jid G4s1919_\${variantdb} -hold_jid G4s2020_\${variantdb} -o \${lg}/\${variantdb}/g2121_\${variantdb}_\\\$JOB_ID.log -e \${lg}/\${variantdb}/g2121_\${variantdb}_\\\$JOB_ID.error -m ea -M \${mail} -cwd -l h_vmem=\${sge_m_j1} -q \${sge_q} 
+
+echo
+echo " --- END PIPELINE ---"
+
+exit
+
+
+EOF
+
+}
