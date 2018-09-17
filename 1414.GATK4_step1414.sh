963dea4f98e4ca8b55811bd039d85e6891fe23ce
macsx82 <macsx82@gmail.com>
2018-09-17 04:37:07 +0200

    fixed input error combineGVCF

--- 1414.GATK4_step1414.sh
+++ 1414.GATK4_step1414.sh
@@ -24,13 +24,14 @@ case ${joint_mode} in
         echo
         cd ${fol7}/${variantdb}/
         echo "> GenomicsDBImport"
+        bs=`wc -l ${fol7}/${variantdb}/gVCF.list| cut -f 1 -d " "`
         ${GATK4} --java-options ${java_opt2x} GenomicsDBImport --genomicsdb-workspace-path ${fol7}/${variantdb}/${f2} --batch-size ${bs} -L "${f1}" --sample-name-map ${fol7}/${variantdb}/gVCF.list --reader-threads ${rt} -ip ${ip2}
         echo "- END -"
     ;;
     GENO )
         echo
         # cd ${fol7}/${variantdb}/
-        samples_list=`find ${fol6}/*_g.vcf.gz -type f -printf "%f\n" | awk -v base_folder=${fol6} '{print base_folder"/"$1}'| tr "\n" " "`
+        samples_list=`find ${fol6}/*_g.vcf.gz -type f -printf "%f\n" | awk -v base_folder=${fol6} '{print "-V "base_folder"/"$1}'| tr "\n" " "`
         echo "> CombineGVCFs"
         ${GATK4} --java-options ${java_opt2x} CombineGVCFs -O ${fol7}/${variantdb}/${f2}_g.vcf.gz -R ${GNMhg38} -L "${f1}" -V ${samples_list}
         echo "- END -"
