1f34a2e9723f68694f3f370739522297f8804b33
macsx82 <macsx82@gmail.com>
2018-12-29 23:19:53 +0100

    test merge gvcf with gatk

--- Chr1011.GATK4_step1011.sh
+++ Chr1011.GATK4_step1011.sh
@@ -26,7 +26,7 @@ echo "- END -"
 echo
 # cd ${fol5}/
 echo "> MergeVcfs"
-${GATK4} --java-options "${java_opt2x} -XX:+UseSerialGC" MergeVcfs -I "${fol5}/${SM}.list" -O ${fol6}/${gVCF}
+${GATK4} --java-options "${java_opt2x} -XX:+UseSerialGC" MergeVcfs -I "${fol6}/${SM}.list" -O ${fol6}/${gVCF}
 #use the bcftools option: faster and without java (which is a drag!!)
 # ${BCFTOOLS} concat -a -f ${fol6}/${SM}.list | ${BCFTOOLS} sort -O z -o ${fol6}/${gVCF}
 
