8490cc12fb65596d3afb5246a9340be6e897ccb8
macsx82 <macsx82@gmail.com>
2019-01-08 18:46:42 +0100

    fixed merge gvcf files with bcftools instead of gatk

--- 1616.GATK4_step1616.sh
+++ 1616.GATK4_step1616.sh
@@ -20,14 +20,29 @@ echo
 # cd ${fol8}/${variantdb}/
 echo "> Raw VCFs ID data"
 # wVCF=`find ${fol8}/${variantdb}/${variantdb}_*.vcf.gz -type f | awk '{print " I="$1}' | tr "\n" "\t" | sed 's/\t / /g'`
-find ${fol8}/${variantdb}/${variantdb}_*.vcf.gz -type f > ${fol8}/${variantdb}/${variantdb}_all_vcf.list
+# find ${fol8}/${variantdb}/${variantdb}_*.vcf.gz -type f > ${fol8}/${variantdb}/${variantdb}_all_vcf.list
+# We need to proceed by steps, splitting we need to and sorting data by chunks of 1000 files each maximum (bcftools limit)
+find ${fol8}/${variantdb}/${variantdb}_*.vcf.gz -type f | split -a 4 --additional-suffix _all_vcf.list -d -l 1000 - ${fol8}/${variantdb}/${variantdb}_  
 echo "- END -"
 
 #16b
 echo
 # cd ${fol8}/${variantdb}/
 echo "> Merge VCFs"
-${BCFTOOLS} concat -a -f ${fol8}/${variantdb}/${variantdb}_all_vcf.list | ${BCFTOOLS} sort -T ${tmp} -O z -o ${fol9}/${variantdb}/${raw}
+# ${BCFTOOLS} concat -a -f ${fol8}/${variantdb}/${variantdb}_all_vcf.list | ${BCFTOOLS} sort -T ${tmp} -O z -o ${fol9}/${variantdb}/${raw}
+for par_file in ${fol8}/${variantdb}/${variantdb}_*_all_vcf.list
+do
+par_file_name=`basename ${par_file}`
+${BCFTOOLS} concat -a -f ${par_file} | ${BCFTOOLS} sort -T ${tmp} -O z -o ${fol9}/${variantdb}/${par_file_name}.vcf.gz
+tabix -f -p vcf ${fol9}/${variantdb}/${par_file_name}.vcf.gz
+
+done
+
+pVCF=$(find ${fol9}/${variantdb}/${variantdb}_*_all_vcf.list.vcf.gz -type f)
+
+${BCFTOOLS} concat -a ${pVCF} | ${BCFTOOLS} sort -T ${tmp} -O z -o ${fol9}/${variantdb}/${raw}.gz
+tabix -f -p vcf ${fol9}/${variantdb}/${raw}.gz
+
 # java -XX:+UseSerialGC -jar ${PICARD} GatherVcfs ${wVCF} O=${fol9}/${variantdb}/${raw}
 echo "- END -"
 
