4c2bc9a2c3a111314963d032f6ce853362cf5e0b
macsx82 <macsx82@gmail.com>
2019-10-02 21:14:47 +0200

    fixed scripts folder reference in conf generator for pre-alignment mode

--- data_prep_config_file_creation.sh
+++ data_prep_config_file_creation.sh
@@ -73,7 +73,7 @@ ph5=/share/apps/bio/miniconda2/bin/trim_galore    #
 
 ### - PATH SCRIPT / Log - ###
 lg=\${base_out}/Log
-hs=\${base_out}/0.pipe
+hs=\${HOME}/scripts/pipelines/VariantCalling
 
 EOF
 
