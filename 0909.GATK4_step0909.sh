10796cc4b9d321aacec5b94969e7bef73f56030b
macsx82 <macsx82@gmail.com>
2020-01-13 23:30:08 +0100

    added handling of sex chromosomes during calling  pipeline step 9 on cineca

--- 0909.GATK4_step0909.sh
+++ 0909.GATK4_step0909.sh
@@ -112,8 +112,6 @@ if [[ -z "${fol4_tmp}" || -z "${fol5_tmp}" || -z "${fol5_host}" ]];then
 				${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4}/${applybqsr} -O ${fol5}/${c_gv} -L "${f1}" -ip ${ip1} -ploidy ${sex} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
 				;;
 		esac
-
-
 		# ${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4}/${applybqsr} -O ${fol5}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
 	fi
 	
@@ -141,7 +139,69 @@ else
 	# cd ${fol4}/
 	echo "> HaplotypeCaller"
 	# ${GATK4} --java-options ${java_opt2x} ${java_XX1} ${java_XX2} HaplotypeCaller -R ${GNMhg38} -I ${fol4}/${applybqsr} -O ${fol5}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF
-	${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4_tmp}/${applybqsr} -O ${fol5_tmp}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
+	#We need to manage also the ploidy based on the sex and the interval
+    if [[ ${sex} -eq 2 ]]; then
+        #we are working with female samples: no need to worry about ploidy
+        #but we need to skip chrY calls
+        if [[ -f "${f1}" ]]; then
+            #if the interval is a file, we need to check if its a chrY file 
+            #and skip the submission
+            chrY_content=$(awk '$1~"Y" {print $1}' ${f1} | sort|uniq| wc -l| cut -f 1 -d " ")
+        else
+            #If the interval is submitted in the form of
+            # f1="chrY:2781480-9046914"
+            chrY_content=$(echo ${f1} | awk '$0~"Y"'| wc -l | cut -f 1 -d " ")
+        fi
+
+        if [[ ${chrY_content} -eq 0 ]]; then
+            #we will submit the job
+            echo "Job submitted for female sample on non Y chromosome."
+            ${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4_tmp}/${applybqsr} -O ${fol5_tmp}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
+        fi
+    else
+        #we are working with male samples: we need to worry about ploidy
+        #to correctly handle non par regions and chrY calls
+        if [[ -f "${f1}" ]]; then
+            #if the interval is a file, we need to check if its a chrY file 
+            #and skip the submission
+            chrY_content=$(awk '$1~"Y" {print $1}' ${f1} | sort|uniq| wc -l| cut -f 1 -d " ")
+            chrX_content=$(awk '$0~"NON_PAR"' ${f1}| sort|uniq| wc -l| cut -f 1 -d " ")
+        else
+            #If the interval is submitted in the form of
+            # f1="chrY:2781480-9046914"
+            chrY_content=$(echo ${f1} | awk '$0~"Y"'| wc -l | cut -f 1 -d " ")
+            #for chrX interval, we need to check if it is in the par or non par regions
+            # f1="chrX:10001-2781479"
+            #X: 10001 - 2781479 (PAR1)
+            #X: 155701383 - 156030895 (PAR2)
+            #we are checking if we are outside the par regions
+            chrX_content=$( echo ${f1} | awk '$0~"X"' | awk '{split($1,b,":");split(b[2],c,"-");if( (c[1] <= 10001 || (c[1] >= 2781479 && c[1] <= 155701383) || c[1] >= 156030895) && (c[2] <= 10001 ||( c[2] >= 2781479 && c[2] <= 155701383)|| c[2] >= 156030895) ) print $0}' | wc -l | cut -f 1 -d " ")
+        fi
+        if [[ ${chrY_content} != "0" ]]; then
+            chrom_status="SEXC"
+        fi
+        if [[ ${chrX_content} != "0" ]]; then
+            chrom_status="SEXC"
+        fi
+        if [[ ${chrX_content} -eq 0 && ${chrY_content} -eq 0 ]]; then
+            chrom_status="AUTOSOMAL"
+        fi
+        # echo ${chrom_status}
+        case ${chrom_status} in
+            AUTOSOMAL )
+                echo "Job submitted for male sample on autosomal chromosome or chrX PAR regions."
+                ${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4_tmp}/${applybqsr} -O ${fol5_tmp}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
+                ;;
+            SEXC )
+                echo "Job submitted for male sample on Y chromosome or chrX NON_PAR regions."
+                ${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4_tmp}/${applybqsr} -O ${fol5_tmp}/${c_gv} -L "${f1}" -ip ${ip1} -ploidy ${sex} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
+                ;;
+        esac
+        # ${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4}/${applybqsr} -O ${fol5}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
+    fi
+    
+    echo "- END -"
+    # ${GATK4} --java-options "${java_opt2x} ${java_XX1} ${java_XX2}" HaplotypeCaller -R ${GNMhg38} -I ${fol4_tmp}/${applybqsr} -O ${fol5_tmp}/${c_gv} -L "${f1}" -ip ${ip1} --max-alternate-alleles ${maa} -ERC GVCF --output-mode EMIT_ALL_CONFIDENT_SITES
 	echo "- END -"
 
 	#qdel
