c2c4e335363294fb55bd5f73dc74be631ce1bc69
macsx82 <macsx82@gmail.com>
2020-03-28 23:22:34 +0100

    ..and added step 14 in runner

--- f_build_runner_GDBIMP.sh
+++ f_build_runner_GDBIMP.sh
@@ -92,25 +92,13 @@ case \${cluster_man} in
 				#pipe step 14, job-array
 				#IN gCVF (gVCF-list) OUT gVCFDB /// GenomicsDBImport
 				#We will need to work with a job array by chromosome
-				for chr in \${chr_pool[@]}
-                do
-
-                done
-
-				#we need to split the interval file by 100 lines, but we need to do it on the fly
-				#than we nedd to collect each interval file path in another file and run a job array of intervals
-				#we should end up with a job array with ~2K tasks each with a file of 100 intervals
-				mkdir -p \${tmp}/db_imp_int
-				cd \${tmp}/db_imp_int
-
-				split -a 4 --additional-suffix dbImp.intervals -d -l 100 \${vdb_interval}
-
-				ls \${tmp}/db_imp_int/x*dbImp.intervals > \${tmp}/db_imp_int/ALL_dbImp.intervals
-
-				# rsync -av -u -P R \${tmp}/db_imp_int ${USER}@${exec_host}:/.
+				#we will have one array with 25 tasks, each passing a file containing intervals for DBimport
+				size=\$(wc -l \${vdb_interval}|cut -f 1 -d " ")
+				jid_step_1414_m=\$(sbatch --partition=\${sge_q} --account=uts19_dadamo --array=1-\${size} --time=24:00:00 -e \${lg}/%A_g1414_\${SM}_%a.error -o \${lg}/%A_g1414_\${SM}_%a.log --mem=\${sge_m} -J "G4s1414_\${SM}" --dependency=afterok:\${jid_step_1313} --get-user-env -n 1 --mail-type END,FAIL --mail-user \${mail} \${hs}/runner_job_array_CINECA.sh -s \${hs}/1414.GATK4_step1414.sh \${vdb_interval} \${param_file}
+                jid_step_1414=\$(echo \${jid_step_1414_m}| cut -f 4 -d " ")
 
 
-				a_size=\`wc -l \${tmp}/db_imp_int/ALL_dbImp.intervals | cut -f 1 -d " "\`; echo "\${hs}/runner_job_array.sh -s \${hs}/1414.GATK4_step1414.sh \${tmp}/db_imp_int/ALL_dbImp.intervals \${param_file}" | qsub -t 1-\${a_size} -tc 15 -N G4s1414_\${variantdb}_ -cwd -l h_vmem=\${sge_m_dbi} -hold_jid G4s1313_\${variantdb} -o \${lg}/\${variantdb}/g1414_\\\$JOB_ID.\\\$TASK_ID.log -e \${lg}/\${variantdb}/g1414_\\\$JOB_ID.\\\$TASK_ID.error -m ea -M \${mail} -q \${sge_q}
+
             ;;
             SAMPLE)
                 jid_step_1011_m=\$(sbatch --partition=\${sge_q} --account=uts19_dadamo --time=24:00:00 -e \${lg}/%j_g1011_\${SM}.error -o \${lg}/%j_g1011_\${SM}.log --mem=\${sge_m} -J "G4s1011_\${SM}" --get-user-env -n 1 --mail-type END,FAIL --mail-user \${mail} \${hs}/sample1011.GATK4_step1011.sh \${param_file})
