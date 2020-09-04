841355d46f1eafe0b04a9c4646a56358519baa00
macsx82 <macsx82@gmail.com>
2020-09-04 15:51:50 +0200

    removed chr merging from step 1919

--- 1919.GATK4_step1919.sh
+++ 1919.GATK4_step1919.sh
@@ -14,23 +14,6 @@ source ${param_file}
 own_folder=`dirname $0`
 source ${hs}/pipeline_functions.sh
 ### - CODE - ###
-case ${pool_mode} in
-    CHROM)
-	#19a
-	#First collect all vcf files without genotypes and merge them
-	#I need generate the list of files to merge together
-	for current_chr in ${chr_pool[@]}
-	do
-		#we need to collect ALL chromosomes to merge them. we are not working by chr at this point
-		int_vcf="${fol8}/${SO}_${current_chr}.vcf.gz"
-		find ${int_vcf} -type f 
-	done > ${fol8}/${variantdb}_all_chr_vcf.list
-	
-	echo -e "\n> Merge sites only VCFs"
-	# concat , sort and normalize indels 
-	${BCFTOOLS} concat -a -f ${fol8}/${variantdb}_all_chr_vcf.list | ${BCFTOOLS} sort -T ${tmp} | ${BCFTOOLS} norm -f ${GNMhg38} -O z -o ${fol8}/${SO}.vcf.gz
-	;;
-esac
 
 #19
 echo
